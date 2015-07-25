fa0cc1f2210a1fa7ab23d2565d0799d298a7e461
Hoangdv <hoang.dv@outlook.com>
2015-07-25 21:21:39 +0700

    Start git

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_add = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var io = require('socket.io')(http);
+io.on('connection', function (socket) {
+	console.log(socket.id + ' is connected!');
+	socket.on('push', function (data) {
+		console.log(data);
+		io.emit('sendmsg', {hash_sec: data.hash_sec, to: data.to, msg: data.msg});
+	});
+	socket.on('disconnect', function(){
+		console.log(socket.id+' is disconnected!');
+	});
+});
+http.listen(port, server_ip_add, function (){
+	console.log('Server listening on '+server_ip_add+':'+port);
+});
