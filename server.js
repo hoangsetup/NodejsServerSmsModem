a6577919409dcc17f807bfbf7fa49073d0ac0b2b
Hoangdv <hoang.dv@outlook.com>
2015-07-25 23:23:23 +0700

    add config

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var io = require('socket.io')(http, {
     'origins': 'https://'+server_ip_add+':'+port,
     'serveClient': false
 });
+io.set('origins', '*:*');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
